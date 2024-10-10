cf91b5e8cf5ab6e3d395787de3ee835cdba5bafb
Juri <juri100g@gmail.com>
2024-10-10 13:02:59 +0200

    chore: general setup

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+              src/app/page.tsx
             </code>
             .
           </li>
